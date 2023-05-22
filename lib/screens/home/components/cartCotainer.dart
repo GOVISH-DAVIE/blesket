d9425e8b0f5f4c0930e0c84cdb1590136189ea4a
Govish-Davie <9davidmuia@gmail.com>
2023-05-22 14:48:54 +0300

    fin

--- lib/screens/home/components/cartCotainer.dart
+++ lib/screens/home/components/cartCotainer.dart
@@ -24,39 +24,41 @@ class _CartContainerState extends State<CartContainer> {
   @override
   Widget build(BuildContext context) {
     return Consumer<ProductProvider>(
-      builder: (context, productProvider, child) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          context.read<SocketsProvider>().isConnected == true
-              ? Container(
-                  height: 20,
-                  width: 20,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.green),
-                )
-              : Row(
-                  children: [
-                    Container(
+      builder: (context, productProvider, child) => Consumer<SocketsProvider>(
+        builder:(context, socketProvider, child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              context.read<SocketsProvider>().isConnected == true
+                  ? Container(
                       height: 20,
                       width: 20,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                          color: dustred),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left:8.0),
-                      child: FullWithButton(
-                          callback: () {
-                            context.read<SocketsProvider>().connect(
-                                signalingUrl: signalingEndPoint,
-                                context: context);
-                          },
-                          type: outlineButtonDefault,
-                          child: const Text('connect socket')),
+                          color: Colors.green),
                     )
-                  ],
-                ),
+                  : Row(
+                      children: [
+                        Container(
+                          height: 20,
+                          width: 20,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: dustred),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: FullWithButton(
+                              callback: () {
+                                context.read<SocketsProvider>().connect(
+                                    signalingUrl: signalingEndPoint,
+                                    context: context);
+                              },
+                              type: outlineButtonDefault,
+                              child: const Text('connect socket')),
+                        )
+                      ],
+                    ),
 
 //           FullWithButton(
 //               callback: () {
@@ -74,139 +76,148 @@ class _CartContainerState extends State<CartContainer> {
 //               type: outlineButtonDefault,
 //               child: const Text('simulate handle mesage')),
 
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'My Cart',
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-            ),
-          ),
-          Expanded(
-              child: SingleChildScrollView(
-            child: context.watch<ProductProvider>().cartLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : Column(
-                    children: [
-                      ...?productProvider.cartListModel?.myCart?.map(
-                        (e) => Dismissible(
-                          confirmDismiss: (DismissDirection direction) async {
-                            return await showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  backgroundColor: dustred,
-                                  title: const Text("Cart "),
-                                  content: const Text(
-                                      "Are you sure you want to remove this item from the cart?"),
-                                  actions: <Widget>[
-                                    FullWithButton(
-                                        type: defaultButtonTheme,
-                                        callback: () {
-                                          context
-                                              .read<ProductProvider>()
-                                              .removeFromCart(
-                                                  productSlung: productProvider
-                                                      .productLists
-                                                      .where((element) =>
-                                                          element.id ==
-                                                          e.product)
-                                                      .first
-                                                      .slug!,
-                                                  cartItemId: e.id!,
-                                                  context: context);
-                                        },
-                                        // Navigator.of(context).pop(true),
-                                        child: const Text("Yes")),
-                                    FullWithButton(
-                                      type: inactiveButtonTheme,
-                                      callback: () =>
-                                          Navigator.of(context).pop(false),
-                                      child: const Text("No"),
-                                    ),
-                                  ],
+              Row(
+                children: [
+                  Text('Current Weight:\t'),
+                  Text('${context.read<SocketsProvider>().currentWeight}'),
+                ],
+              ),
+
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'My Cart',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                ),
+              ),
+              Expanded(
+                  child: SingleChildScrollView(
+                child: context.watch<ProductProvider>().cartLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : Column(
+                        children: [
+                          ...?productProvider.cartListModel?.myCart?.map(
+                            (e) => Dismissible(
+                              confirmDismiss: (DismissDirection direction) async {
+                                return await showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      backgroundColor: dustred,
+                                      title: const Text("Cart "),
+                                      content: const Text(
+                                          "Are you sure you want to remove this item from the cart?"),
+                                      actions: <Widget>[
+                                        FullWithButton(
+                                            type: defaultButtonTheme,
+                                            callback: () {
+                                              context
+                                                  .read<ProductProvider>()
+                                                  .removeFromCart(
+                                                      productSlung: productProvider
+                                                          .productLists
+                                                          .where((element) =>
+                                                              element.id ==
+                                                              e.product)
+                                                          .first
+                                                          .slug!,
+                                                      cartItemId: e.id!,
+                                                      context: context);
+                                            },
+                                            // Navigator.of(context).pop(true),
+                                            child: const Text("Yes")),
+                                        FullWithButton(
+                                          type: inactiveButtonTheme,
+                                          callback: () =>
+                                              Navigator.of(context).pop(false),
+                                          child: const Text("No"),
+                                        ),
+                                      ],
+                                    );
+                                  },
                                 );
                               },
-                            );
-                          },
-                          key: Key('item ${e.toJson()}'),
-                          child: SizedBox(
-                            width: MediaQuery.of(context).size.width / (2.1),
-                            height: 150,
-                            child: InkWell(
-                              onTap: () {
-                                productDialogBuilder(
-                                    context,
-                                    productProvider.productLists
-                                        .where((element) =>
-                                            element.id == e.product)
-                                        .first,
-                                    true,
-                                    white);
-                              },
-                              child: Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: SizedBox(
-                                      width: 100,
-                                      height: 200,
-                                      child: Image.network(
-                                        width: 100,
-                                        height: 200,
-                                        fit: BoxFit.cover,
-                                        // ProductEndPoints.imageLink
+                              key: Key('item ${e.toJson()}'),
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width / (2.1),
+                                height: 150,
+                                child: InkWell(
+                                  onTap: () {
+                                    productDialogBuilder(
+                                        context,
                                         productProvider.productLists
                                             .where((element) =>
                                                 element.id == e.product)
-                                            .first
-                                            .mobileImage!,
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                      child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                                            .first,
+                                        true,
+                                        white);
+                                  },
+                                  child: Row(
                                     children: [
-                                      Text(
-                                        '${productProvider.productLists.where((element) => element.id == e.product).first.productName}',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyLarge,
+                                      Padding(
+                                        padding: const EdgeInsets.only(bottom: 8.0),
+                                        child: SizedBox(
+                                          width: 100,
+                                          height: 200,
+                                          child: Image.network(
+                                            width: 100,
+                                            height: 200,
+                                            fit: BoxFit.cover,
+                                            // ProductEndPoints.imageLink
+                                            productProvider.productLists
+                                                .where((element) =>
+                                                    element.id == e.product)
+                                                .first
+                                                .mobileImage!,
+                                          ),
+                                        ),
                                       ),
-                                      Text(
-                                        ' Quantity : ${e.quantity}',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyMedium,
-                                      ),
+                                      Expanded(
+                                          child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            '${productProvider.productLists.where((element) => element.id == e.product).first.productName}',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyLarge,
+                                          ),
+                                          Text(
+                                            ' Quantity : ${e.quantity}',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyMedium,
+                                          ),
+                                        ],
+                                      )),
+                                      SizedBox(
+                                        width: 100,
+                                        child: Text(
+                                          'Ksh ${productProvider.productLists.where((element) => element.id == e.product).first.variation?.where((element) => element.id == e.variations).first.price}',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                      )
                                     ],
-                                  )),
-                                  SizedBox(
-                                    width: 100,
-                                    child: Text(
-                                      'Ksh ${productProvider.productLists.where((element) => element.id == e.product).first.variation?.where((element) => element.id == e.variations).first.price}',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                  )
-                                ],
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 18.0, right: 18),
+                            child: Divider(),
+                          ),
+                        ],
                       ),
-                      const Padding(
-                        padding: EdgeInsets.only(left: 18.0, right: 18),
-                        child: Divider(),
-                      ),
-                    ],
-                  ),
-          ))
-        ],
+              ))
+            ],
+          );
+        }
       ),
     );
   }
